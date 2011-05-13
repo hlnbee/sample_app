ec1f48345c616da1b5d98675ad5de97db0729baf
hlnbee <helen.burgess@staff.central.wa.edu.au>
2011-05-13 09:43:55 +0800

    Added RSpec files'

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '3e4bb2d8433f1ea19de97cdd1a2e2826ed634c5d42c0f6f99d633509bb1754743fb5d9ba53971018d12b865c89e3315df4301a0de009d9da6fad03595758ab8a';
