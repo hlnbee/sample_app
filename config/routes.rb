230bf1ae6eb26421b1a53ad6a03c2d906f546b08
hlnbee <helen.burgess@staff.central.wa.edu.au>
2011-05-13 14:51:17 +0800

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.contact '/contact', :controller => 'pages', :action => 'contact'
-  map.about '/about', :controller => 'pages', :action => 'about'
-  map.help '/help', :controller => 'pages', :action => 'help'
-  
-  map.root :controller => 'pages' :action => 'home'
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -13,7 +9,13 @@ ActionController::Routing::Routes.draw do |map|
   # Sample of named route:
   #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
   # This route can be invoked with purchase_url(:id => product.id)
-
+  #map.index 'home', :controller => 'pages', :action => 'home'
+  map.root :controller => 'pages', :action => 'home'
+  map.home '/', :controller => 'pages', :action =>'home'
+  map.contact '/contact', :controller => 'pages', :action => 'contact'
+  map.about '/about', :controller => 'pages', :action => 'about'
+  map.help '/help', :controller => 'pages', :action => 'help'
+  map.signup '/signup', :controller=> 'users', :action => 'new'
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   map.resources :products
 
@@ -43,6 +45,9 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+   map.connect ':controller/:action/:id'
+   map.connect ':controller/:action/:id.:format'
+
+  
+  
 end
